d90274002a6306c2c3d2e55db77c7c5f2efda867
Moisés Alcocer <m_alc1@hotmail.com>
2016-10-19 10:31:27 +0200

    Add files via upload

--- DI/series/src/series/test/TestCreate.java
+++ DI/series/src/series/test/TestCreate.java
@@ -7,6 +7,7 @@
 package series.test;
 
 //imports
+import series.helpers.*;
 import series.model.DataStoreHandler;
 import series.model.Show;
 import series.model.ShowsList;
@@ -32,22 +33,32 @@ public class TestCreate {
          * 
          */
         public TestCreate() {
-        
-            //Create a show
-            Show s = new Show(
-                "Mi título",
-                "Comedia",
-                3,
-                false,
-                false, 
-                (float) 1.07,
-                "dvd"
-            );
+            Trace.ln( "TestCreate / construct()" );
+
             
+            //Create a show
+            Trace.ln( "Intanciando Show con todos los parámetros" );
+            Show s = new Show(
+                "Mi título",    // String title
+                "Comedia",      // String gender
+                "AMB",          // String producer
+                3,              // int seasons
+                (float) 1.07,   // float last_episode_viewed
+                "dvd",          // String store_media
+                false,          // boolean cancelled
+                false,          // boolean completed
+                5               // int rating
+            );
+
+
             //Add show to list
+            Trace.ln( "Show creado. Añadiendo a ShowsList..." );
             sl.add( s );
 
+            Trace.ln( "-------------------------------------" );
+
             //Create another show
+            Trace.ln( "Intanciando Show con 3 parámetros" );
             s = new Show(
                 "Mi otro título",
                 "Acción",
@@ -55,9 +66,24 @@ public class TestCreate {
             );
 
             //Add another show to list
+            Trace.ln( "Show creado. Añadiendo a ShowsList..." );
             sl.add( s );
 
+            Trace.ln( "-------------------------------------" );
+
+            //Create another show
+            Trace.ln( "Intanciando Show sin parámetros" );
+            s = new Show();
+
+            //Add another show to list
+            Trace.ln( "Show creado. Añadiendo a ShowsList..." );
+            sl.add( s );
+            /**/
+            Trace.ln( "-------------------------------------" );
+            Trace.ln( "-------------------------------------" );
+
             //Store data
+            Trace.ln( "Guardando ShowsList en fichero..." );
             DataStoreHandler.store( sl );
             
             //Display succesful message
