9dda83163bdf1911e21a427d55d94d05b11af8b9
Moisés Alcocer <m_alc1@hotmail.com>
2016-11-12 00:01:26 +0100

    Created skeleton for java apps

---
+++ Java%20App%20Skeleton/app/model/Model.java
@@ -0,0 +1,43 @@
+/**
+ * @file: Model.php
+ * 
+ * @utor: Moisés Alcocer, 2016
+ */
+
+package app.model;
+
+//imports
+
+
+/**
+ * This class ...
+ * 
+ */
+public class Model {
+
+    /**********************************/
+    /*** Properties declaration *******/
+
+
+
+        
+    /**********************************/
+    /*** Methods declaration **********/
+
+	    /**
+	     * Method description
+	     *
+	     * @param 	str
+	     */
+	    public void xxx( String str ) {
+
+	        // Code here
+	    }
+
+
+    /**********************************/
+    /*** Internal clases **************/
+
+    
+
+} //class
