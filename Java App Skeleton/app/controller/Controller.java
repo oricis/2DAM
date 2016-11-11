25cfe9a1b7fe11b21b1c24048edb17cf407c3520
Moisés Alcocer <m_alc1@hotmail.com>
2016-11-11 23:31:29 +0100

    First commit - Created skeleton structure

---
+++ Java App Skeleton/app/controller/Controller.java
@@ -0,0 +1,57 @@
+/**
+ * @file: Controller.php
+ * 
+ * @utor: Moisés Alcocer, 2016
+ */
+
+package app.controller;
+
+//imports
+import app.model.Model;
+import app.view.View;
+
+
+
+/**
+ * This class ...
+ * 
+ */
+public class Controller {
+
+    /**********************************/
+    /*** Properties declaration *******/
+
+        private View v  = null;
+        private Model m = null;
+
+        
+    /**********************************/
+    /*** Methods declaration **********/
+
+	    /**
+	     * Construct
+	     *
+	     */
+	    public Controller() {
+
+	        v = new View();
+                m = new Model();
+	    }
+            
+            /**
+	     * Method description
+	     *
+	     * @param 	str
+	     */
+	    public void xxx( String str ) {
+
+	        // Code here
+	    }
+
+
+    /**********************************/
+    /*** Internal clases **************/
+
+    
+
+} //class
---
+++ Java App Skeleton/app/view/View.java
@@ -0,0 +1,43 @@
+/**
+ * @file: View.php
+ * 
+ * @utor: Moisés Alcocer, 2016
+ */
+
+package app.view;
+
+//imports
+
+
+/**
+ * This class ...
+ * 
+ */
+public class View {
+
+    /**********************************/
+    /*** Properties declaration *******/
+
+
+
+        
+    /**********************************/
+    /*** Methods declaration **********/
+
+	    /**
+	     * Method description
+	     *
+	     * @param 	str
+	     */
+	    public void xxx( String str ) {
+
+			// Code here
+	    }
+
+
+    /**********************************/
+    /*** Internal clases **************/
+
+
+
+} //class
