86973cc8b96dfa732a2fdbd373eab023c27c2d9b
Moisés Alcocer <m_alc1@hotmail.com>
2016-11-12 11:39:12 +0100

    Add files via upload

---
+++ DI/App8/view/Frame.java
@@ -0,0 +1,57 @@
+/**
+ * @file: Frame.php
+ * 
+ * @utor: Moisés Alcocer, 2016
+ */
+
+package app.view;
+
+//imports
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ * This class handle App GUI functionalities
+ * 
+ */
+public class Frame extends LayoutComposer {
+
+    /**********************************/
+    /*** Properties declaration *******/
+
+
+
+        
+    /**********************************/
+    /*** Methods declaration **********/
+
+	    /**
+	     * Construct
+	     *
+	     */
+	    public Frame() {
+
+			
+	    }
+
+
+    /**********************************/
+    /*** Internal clases **************/
+
+        private class AppListener implements ActionListener {
+
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                
+                //
+                // Put your code here
+                //
+                //
+                //
+
+            }
+            
+        } //class
+        
+} //class
