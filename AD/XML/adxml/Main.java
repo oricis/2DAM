730cc4e32436e97d9f974a46a3cd4c1bd232d05d
Moisés Alcocer <m_alc1@hotmail.com>
2016-12-18 17:49:16 +0100

    Update - Se solicita opción / datos al usuario
    Se solicita que ejecutar:
    1. Ver estadísticas jugadores
    2. Subir nivel a jugador
    3. Añadir puntos a jugador
    
    Se solicita nombre jugador para subir de nivel o aumentar puntuación.
    Se solicita puntuación a aumentar si se selecciono esta opción.

--- AD/XML/adxml/Main.java
+++ AD/XML/adxml/Main.java
@@ -7,6 +7,7 @@
 package adxml;
 
 import java.io.IOException;
+import java.util.Scanner;
 import javax.xml.transform.TransformerException;
 
 /**
@@ -18,7 +19,13 @@ public class Main {
 	/**********************************/
 	/*** Properties declaration *******/
 
+		private static int option;
+		private static int score;
+		private static Scanner s;
+		private static String input = "";
+		private static String player;
 	
+
 	/**********************************/
 	/*** Methods declaration **********/
 
@@ -29,16 +36,98 @@ public class Main {
 		 */
 		public static void main( String[] args ) throws IOException, TransformerException {
 			
+			selectOption();
+
 			//new PruebaLeerNodosElementos();
-			
-			new Estadisticas();
-			//new Level().up( "Riu" );	//Ups a level to this player
-			//Trace.ln( "Actualizado nivel para Riu \n" );
-
-			new Score().add( "Riu", 100 );	//Adds score to this player
-			Trace.ln( "Actualizada puntuación de Riu \n" );
-
-			new Estadisticas();
 		}
-	
+		
+		/**
+		 * Selects an options and run the program
+		 * 
+		 */
+		private static void selectOption() {
+
+			showOptions();
+			s     = new Scanner( System.in );
+			input = s.nextLine();
+			System.out.println ( "Opción seleccionada: " + input );
+
+			try {
+
+				option = Integer.parseInt( input );
+				
+				//Show statistiques
+				if ( option == 1 )
+					new Estadisticas();
+
+				else {
+					//Selects a player
+					selectPlayer();
+
+					//Ups a level
+					if ( option == 2 ) {
+						new Level().up( player );	//Ups a level to this player
+						Trace.ln( "Actualizado nivel de \"" + player + "\"\n" );
+
+					//Adds score
+					} else if ( option == 3 ) {
+
+						selectScore();
+						new Score().add( player, score );	//Adds score to this player
+						Trace.ln( "Actualizada puntuación de \"" + player + "\"\n" );
+					}
+				}
+
+			} catch( Exception e ) {
+
+				System.out.println( "Selección incorrecta" );
+				selectOption();
+			}
+		}
+
+		/**
+		 * Selects a player
+		 * 
+		 */
+		private static void selectPlayer() {
+			System.out.println( "Escriba el nombre del jugador: " );
+
+			s     = new Scanner( System.in );
+			player = s.nextLine();
+			Trace.ln( "Jugador seleccionado: " + input );
+		}
+
+		/**
+		 * Selects a score
+		 * 
+		 */
+		private static void selectScore() {
+			System.out.println( "Escriba puntuación a añadir: " );
+
+			s     = new Scanner( System.in );
+			input = s.nextLine();
+			Trace.ln( "Puntuación a añadir: " + input );
+
+			try {
+				score = Integer.parseInt( input );
+
+			} catch( Exception e ) {
+
+				System.out.println( "Selección score incorrecta" );
+				selectScore();
+			}
+		}
+
+		/**
+		 * Shows the options list
+		 * 
+		 */
+		private static void showOptions() {
+
+			System.out.println( "Escriba el número de opción seleccionada: " );
+			System.out.println( "   1. Ver estadisticas jugadores" );
+			System.out.println( "   2. Subir nivel a jugador" );
+			System.out.println( "   3. Añadir puntos a jugador" );
+		}
+
 } //class
